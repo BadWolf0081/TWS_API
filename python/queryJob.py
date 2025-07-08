c4539432973d1ed1d80e55262f27c26e6358aa4c
Robbie <57416363+BadWolf0081@users.noreply.github.com>
2025-07-08 14:00:12 -0300

    job?

---
+++ python/queryJob.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import waconn
+import argparse
+
+parser = argparse.ArgumentParser(description='Query job.')
+parser.add_argument('-j','--jname', help='job name filter', required=True, metavar="J_FILTER")
+
+args = parser.parse_args()
+conn = waconn.WAConn('waconn.ini','/twsd')
+
+# Query to find pools matching provided filter
+resp = conn.post('/plan/job',
+	{ "filters": { "jobInPlanFilter": { "jobName": args.jsname } } }, 
+	headers={'How-Many': '500'})
+
+r = resp.json()
+
+#print json.dumps(r, indent=2)
+for js in r:
+    print(js["key"]["workstationKey"]["name"]+'#'+js["key"]["name"]+'('+js["key"]["startTime"]+')')
+
